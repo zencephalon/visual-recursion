6e0115d14a1f58985550fa0b2ca0e8130198f46a
Matthew Bunday <zenceph@gmail.com>
2014-05-15 10:56:48 -0400

    File with some nice recursive stuff.

---
+++ recur.rb
@@ -0,0 +1,70 @@
+
+def count_down_from(num)
+  puts num
+
+  return if num == 1
+
+  count_down_from(num - 1)
+end
+
+def count_up_to(num)
+  return if num == 0
+
+  count_up_to(num - 1)
+
+  puts num
+end
+
+def recur_until_depth(num, depth)
+  puts "#"*(depth) + " BEFORE: stack level #{depth}"
+
+  recur_until_depth(num - 1, depth + 1) if num > 0
+
+  puts "#"*(depth) + " AFTER : stack level #{depth}"
+end
+
+def recur(num)
+  recur_until_depth(num, 0)
+end
+
+def puts_at_depth(depth, str)
+  puts "   "*(depth) + str
+end
+
+def fib(n)
+ return 1 if n == 0
+ return 1 if n == 1
+ return fib(n - 1) + fib(n - 2)
+end
+
+def fib_recur(n, depth)
+  if n == 0
+    puts_at_depth(depth, "BASE  : 0 Returning 1")
+    return 1
+  end
+  if n == 1
+    puts_at_depth(depth, "BASE  : 1 Returning 1")
+    return 1
+  end
+  puts_at_depth(depth, "#{n} BEFORE:")
+  a = fib_recur(n - 1, depth + 1)
+  puts_at_depth(depth, "#{n} MIDDLE:")
+  b = fib_recur(n - 2, depth + 1)
+  puts_at_depth(depth, "#{n}  AFTER: Returning #{a + b}")
+  return a + b
+end
+
+def fib_wrap(n)
+  fib_recur(n, 0)
+end
+
+
+
+count_down_from(10)
+puts "----------"
+count_up_to(7)
+puts "----------"
+recur(5)
+
+puts "----------"
+fib_wrap(5)
